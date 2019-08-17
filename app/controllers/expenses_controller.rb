36a2e59c2039d5fb6e82edc3a681217542e7d319
Eric Suescun <edsuescun@gmail.com>
2019-08-16 22:41:33 -0500

    Se rediseña el filtrado por mes usando AJAX y partials de index y expenses. Por el momento no queda ordenada la lista si se edita o se crea un nuevo Expense

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,18 +2,18 @@ class ExpensesController < ApplicationController
   
   def index
     @tab = :expenses
-    if params[:year].blank? && params[:month].blank?
-      date_range = Date.today.beginning_of_month..Date.today.end_of_month
-    else
-      date_range = Date.new(params[:year].to_i,params[:month].to_i,1).beginning_of_month..Date.new(params[:year].to_i,params[:month].to_i,1).end_of_month
-    end
+    # if params[:year].blank? && params[:month].blank?
+    #   params[:year] = Date.today.year
+    #   params[:month] = Date.today.month
+    # end
+    date_range = Date.new(params[:year].to_i,params[:month].to_i,1).beginning_of_month..Date.new(params[:year].to_i,params[:month].to_i,1).end_of_month
 
     @expensesNow = Expense.where(:exdate =>  date_range).order('exdate DESC')
     @expenses = Expense.order('exdate DESC')
     respond_to do |format|
       format.html
-      # format.js
-      format.json { render :json => @expenses.order('exdate DESC') }
+      format.js
+      format.json { render :json => @expensesNow.order('exdate DESC') }
     end
 
   end
