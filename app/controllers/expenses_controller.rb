8054fbd6775a08a8b884fbf72c16c81edd13faf5
Eric Suescun <edsuescun@gmail.com>
2019-08-16 23:09:30 -0500

    Refactor con expenses.html.erb e indexbar.html.erb

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,18 +2,18 @@ class ExpensesController < ApplicationController
   
   def index
     @tab = :expenses
-    # if params[:year].blank? && params[:month].blank?
-    #   params[:year] = Date.today.year
-    #   params[:month] = Date.today.month
-    # end
+    if params[:year].blank? && params[:month].blank?
+      params[:year] = Date.today.year
+      params[:month] = Date.today.month
+    end
     date_range = Date.new(params[:year].to_i,params[:month].to_i,1).beginning_of_month..Date.new(params[:year].to_i,params[:month].to_i,1).end_of_month
 
-    @expensesNow = Expense.where(:exdate =>  date_range).order('exdate DESC')
-    @expenses = Expense.order('exdate DESC')
+    @expenses = Expense.where(:exdate =>  date_range).order('exdate DESC')
+    # @expenses = Expense.order('exdate DESC')
     respond_to do |format|
       format.html
       format.js
-      format.json { render :json => @expensesNow.order('exdate DESC') }
+      format.json { render :json => @expenses.order('exdate DESC') }
     end
 
   end
@@ -41,6 +41,7 @@ class ExpensesController < ApplicationController
     @expense.update(expense_params)
     respond_to do |format|
       format.js
+      redirect_to expenses_path
     end
   end
 
