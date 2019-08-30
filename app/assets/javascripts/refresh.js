36828dc86ae55889c002bbf30dc225c049e32bf2
Eric Suescun <edsuescun@gmail.com>
2019-08-30 18:52:47 -0500

    Los filtros de la misma categoria solo se pueden activar cuando se apague alguno que este encendido, igual los de type. Son mutuamente excluyentes, no funciona automáticamente aun.

--- app/assets/javascripts/refresh.js
+++ app/assets/javascripts/refresh.js
@@ -1,6 +1,7 @@
 var url;
 $(document).on("click", 'a[data-remote=true]', function(e) {
-  url = $(this).attr('href');
-  history.pushState(null, '', url);
+	e.preventDefault();
+  	url = $(this).attr('href');
+  	history.pushState(null, '', url); //La variable url esta en el contexto de los data-remote=true, NO funcionara en los de abajo porque no tiene ese contexto.
 });
 
--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -1,2 +1,5 @@
 module ExpensesHelper
+	def removeparam(a,b)
+		return a.gsub(b,'')
+	end
 end
