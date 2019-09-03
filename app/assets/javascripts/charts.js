37807c1813c50b7a6457c155742d6e42dbf9007c
Eric Suescun <edsuescun@gmail.com>
2019-09-03 11:05:21 -0500

    Se implementa el tablero por dias. Igual que el otro tablero con BAR, hay que ajustarle para que tome todos los extypes.

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -1,5 +1,4 @@
 function charts() {
-	console.log('Printing charts!');
 	new Morris.Bar({
 	  // ID of the element in which to draw the chart.
 	  element: 'expenses-chart',
@@ -15,5 +14,29 @@ function charts() {
 	  labels: ['Expense'],
 
 	  stacked: true
+	});
+
+	new Morris.Bar({
+	  // ID of the element in which to draw the chart.
+	  element: 'daily-expenses-chart',
+	  // Chart data records -- each entry in this array corresponds to a point on
+	  // the chart.
+	  data: $('#daily-expenses-chart').data('expenses'),
+	  // The name of the data record attribute that contains x-values.
+	  xkey: 'x',
+	  // A list of names of data record attributes that contain y-values.
+	  ykeys: ['purchase', 'withdrawal'],
+	  // Labels for the ykeys -- will be displayed when you hover over the
+	  // chart.
+	  labels: ['Expense'],
+
+	});
+
+	new Morris.Donut({
+	  // ID of the element in which to draw the chart.
+	  element: 'category-chart',
+	  // Chart data records -- each entry in this array corresponds to a point on
+	  // the chart.
+	  data: $('#category-chart').data('expenses'),
 	});
 }
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -15,6 +15,35 @@ module DashboardHelper
     	end
     	return @data
 	end
+
+		def daily_data
+			date_range = (Date.today.beginning_of_month - 1.month)..(Date.today.end_of_month - 1.month)
+	    	@expenses = Expense.where({:exdate => date_range, :extype => 'purchase' })
+
+	    	@data = [ ]
+	    	
+	    	date_range.each do |d, n|
+	    		row = {}
+	    		row[:x] = d.day
+	    		Expense.extypes.each do |extype, n|
+	    			row[extype.to_s] = Expense.where({exdate: d, extype: extype }).sum(:amount)
+	    		end
+	    		@data.push(row)
+	    	end
+	    	return @data
+		end
+
+	def category_data
+		date_range = (Date.today.beginning_of_month - 5.month)..(Date.today.end_of_month)
+    	@data = [ ]
+		Expense.categories.each do |category, n|
+			row = {}
+			row[:label] = category
+			row[:value] = Expense.where({exdate: date_range, category: category }).sum(:amount)
+			@data.push(row)
+		end
+    	return @data
+	end
 end
 
 
