3db9e075b4d375df60bf07feddd1cb792aec2380
Eric Suescun <edsuescun@gmail.com>
2019-09-03 00:20:30 -0500

    Se implementa el tablero de barras acumuladas por 6 meses. Hay que modificarlo para que acepte más extypes

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,20 @@
 module DashboardHelper
+	def monthly_data
+		date_range = Date.today.beginning_of_month..Date.today
+    	@expenses = Expense.where({:exdate => date_range, :extype => 'purchase' })
+
+    	@data = [ ]
+    	
+    	(0..5).each do |m|
+    		row = {}
+    		row[:x] = (Date.today - (5-m).month).end_of_month.strftime('%b')
+    		Expense.extypes.each do |extype, n|
+    			row[extype.to_s] = Expense.where({exdate: (Date.today.beginning_of_month - (5-m).month)..(Date.today.end_of_month - (5-m).month), extype: extype }).sum(:amount)
+    		end
+    		@data.push(row)
+    	end
+    	return @data
+	end
 end
+
+
