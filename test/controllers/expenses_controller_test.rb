9e2a76ea2c64c66029b97f3a8a4a9146c2a46f19
Eric Suescun <edsuescun@gmail.com>
2019-09-08 20:49:36 -0500

    Se usa la nueva forma de solicitar respuestas por JS, evitando el XSRF con la opción xhr: true en los tests.

--- test/controllers/expenses_controller_test.rb
+++ test/controllers/expenses_controller_test.rb
@@ -6,14 +6,44 @@ class ExpensesControllerTest < ActionDispatch::IntegrationTest
 		@expense = expenses(:one)	#Definido en los fixtures
 	end
 
-	test "Should get Expenses index" do
+	test "Should get Expenses index whit no params at all" do
 		get expenses_path
 		assert_response :success
 		assert_not_nil assigns(:expenses)
 	end
 
-	test "Should get Expenes by month" do
+	test "Should get Expenses by month and year in HTML" do
 		get expenses_path, {params: {month: 3, year: 2019}}
+		assert_response :success
+		assert_not_nil assigns(:expenses)
+	end
+
+	test "Should get Expenses by month, year and extype in HTML" do
+		get expenses_path, {params: {month: 3, year: 2019, extype: 0}}
+		assert_response :success
+		assert_not_nil assigns(:expenses)
+	end
+
+	test "Should get Expenses by month, year, category and extype in HTML" do
+		get expenses_path, {params: {month: 3, year: 2019, extype: 0, category: 0}}
+		assert_response :success
+		assert_not_nil assigns(:expenses)
+	end
+
+	test "Should get Expenses by month and year in JS" do
+		get expenses_path, params: { expense: {month: 3, year: 2019, format: 'js' }}, xhr: true
+		assert_response :success
+		assert_not_nil assigns(:expenses)
+	end
+
+	test "Should get Expenses by month, year and extype in JS" do
+		get expenses_path, params: { expense: {month: 3, year: 2019, extype: 0, format: 'js' }}, xhr: true
+		assert_response :success
+		assert_not_nil assigns(:expenses)
+	end
+
+	test "Should get Expenses by month, year, category and extype in JS" do
+		get expenses_path, params: { expense: {month: 3, year: 2019, extype: 0, category: 0, format: 'js' }}, xhr: true
 		assert_response :success
 		assert_not_nil assigns(:expenses)
 	end
@@ -22,6 +52,12 @@ class ExpensesControllerTest < ActionDispatch::IntegrationTest
 		assert_difference "Expense.count", 1 do
 			post "/expenses", params: { expense: { extype: @expense.extype, category: @expense.category, concept: @expense.concept, exdate: @expense.exdate, amount: @expense.amount }, format: 'js' } #Uso los fixtures
 		end
+	end
+
+	test "should filter by extype" do
+		get expenses_path, {params: {month: 3, year: 2019, extype: 1}}
+		assert_response :success
+		assert_not_nil assigns(:expenses)
 	end
 
 end
