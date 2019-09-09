0b82e25102fccc2f9f3d778d4475da59bd4b930a
Eric Suescun <edsuescun@gmail.com>
2019-09-08 19:57:57 -0500

    Se implementa la prueba de creación de Expense especificando el format: js dentro del hash de los params.

--- test/controllers/expenses_controller_test.rb
+++ test/controllers/expenses_controller_test.rb
@@ -1,9 +1,27 @@
 require 'test_helper'
 
 class ExpensesControllerTest < ActionDispatch::IntegrationTest
-	test "Should get Expenses" do
-		get '/expenses_path'
+
+	setup do
+		@expense = expenses(:one)	#Definido en los fixtures
+	end
+
+	test "Should get Expenses index" do
+		get expenses_path
 		assert_response :success
 		assert_not_nil assigns(:expenses)
 	end
+
+	test "Should get Expenes by month" do
+		get expenses_path, {params: {month: 3, year: 2019}}
+		assert_response :success
+		assert_not_nil assigns(:expenses)
+	end
+
+	test "Should create an expense" do
+		assert_difference "Expense.count", 1 do
+			post "/expenses", params: { expense: { extype: @expense.extype, category: @expense.category, concept: @expense.concept, exdate: @expense.exdate, amount: @expense.amount }, format: 'js' } #Uso los fixtures
+		end
+	end
+
 end
