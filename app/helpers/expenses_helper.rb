c0e6cc499f8beaa1f0606d90d30fc3cad9cb8681
Eric Suescun <edsuescun@gmail.com>
2019-10-04 14:44:48 -0500

    Al capitalizar las keys de los enum ya no tuve que usar unos helpers.

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -2,22 +2,4 @@ module ExpensesHelper
 	def removeparam(a,b)
 		return a.gsub(b,'')
 	end
-
-	def extypeCapitalized
-		str = Expense.extypes.keys
-		strc = []
-		str.each do |n|
-			strc << n.capitalize
-		end
-		return strc
-	end
-
-	def categoryCapitalized
-		str = Expense.categories.keys
-		strc = []
-		str.each do |n|
-			strc << n.capitalize
-		end
-		return strc
-	end
 end
