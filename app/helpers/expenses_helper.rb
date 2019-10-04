fec74ac07d905468503f4ba04bc19db95cfb2561
Eric Suescun <edsuescun@gmail.com>
2019-10-04 14:42:47 -0500

    Se implementa Select con BS 3.3.7 adecuadamente, capitalizando las keys del enum.

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -2,4 +2,22 @@ module ExpensesHelper
 	def removeparam(a,b)
 		return a.gsub(b,'')
 	end
+
+	def extypeCapitalized
+		str = Expense.extypes.keys
+		strc = []
+		str.each do |n|
+			strc << n.capitalize
+		end
+		return strc
+	end
+
+	def categoryCapitalized
+		str = Expense.categories.keys
+		strc = []
+		str.each do |n|
+			strc << n.capitalize
+		end
+		return strc
+	end
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,6 +2,6 @@ class Expense < ApplicationRecord
 	validates :concept, :amount, :exdate, presence: true
 	validates :amount, numericality: { only_integer: true }
 
-	enum extype: { purchase: 0, withdrawal: 1, transfer: 2, payment: 3, check: 4, credit_card1: 5, credit_card2: 6, credit_card3: 7  }
-	enum category: { restaurant: 0, car: 1, food: 2, travel: 3, internet: 4, education: 5, services: 6, fun: 7 }
+	enum extype: { Purchase: 0, Withdrawal: 1, Transfer: 2,Payment: 3, Check: 4, Credit_card1: 5, Credit_card2: 6, Credit_card3: 7  }
+	enum category: { Restaurant: 0, Car: 1, Food: 2, Travel: 3, Internet: 4, Education: 5, Services: 6, Fun: 7 }
 end
